bb8aec9f8f7fba42c6bd4185067455a78abd51bb
Dan Vento <dvento.fin@gmail.com>
2020-05-16 13:48:15 +0200

    first commit

---
+++ Kata1/main.py
@@ -0,0 +1,8 @@
+precio = 3.49
+precio_desc = 0.4 * precio
+
+n_barras_vend = input("Introduzca el número de barras vendidas: ")
+int(n_barras_vend)
+    
+print("Precio habitual: ", precio)
+    
---
+++ Kata3/main.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, CommandHandler
+
+def main():
+# 1100117614:AAEmOIiSmV1ZjoPI5oGY-5gxA6uNZVOJUtE
+
+# open("./bot_token").read()
+    # instanciar updater
+    updater = Updater(token="1100117614:AAEmOIiSmV1ZjoPI5oGY-5gxA6uNZVOJUtE", use_context=True)
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+
+    # new handler
+    updater.dispatcher.add_handler(CommandHandler("repeat", repeat))
+    updater.dispatcher.add_handler(CommandHandler("add", addition))
+    
+    # start asking telegram for notifications
+    updater.start_polling()
+
+    # get stop signals
+    updater.idle()
+
+
+def start(update, context):
+    
+    update.message.reply_text("Hello, I'm a bot!")
+    pass
+
+def repeat(update, context):
+    update.message.reply_text(update.message.text)
+
+def addition(update, context):
+    res = int(context.args[0]) + int(context.args[1])
+    update.message.reply_text("Result is: " + str(res))
+
+main()
